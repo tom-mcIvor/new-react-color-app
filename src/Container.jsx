6888f233e0ec0a28557c139a3996dd5b1bad0f0a
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2022-12-30 02:01:22 +1300

    add

--- src/Container.jsx
+++ src/Container.jsx
@@ -3,8 +3,7 @@
 
 
 
-
-const Container = () => {
+const Container = ({ text }) => {
   return (
     <div id="Container"></div>
   )
--- src/InputBox.jsx
+++ src/InputBox.jsx
@@ -2,18 +2,23 @@
 
 
 
-const InputBox = () => {
+const InputBox = ({ text, handleChange, handleSubmit }) => {
+
+
+
+
   return (
-    <form className='searchForm' onSubmit={(e) => e.preventDefault()}>
-      <label htmlFor="search"></label>
-      <input
-        id='search'
-        type="text"
-        role='searchbox'
-        placeholder="Search Items"
-      />
-
-
+    <form onSubmit={handleSubmit} className='searchForm' >
+      <label htmlFor="search">
+        <input
+          id='search'
+          type="text"
+          role='searchbox'
+          placeholder="Search Items"
+          value={text}
+          onChange={handleChange}
+        />
+      </label>
     </form>
   )
 }
