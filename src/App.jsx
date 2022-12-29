769f08310de387d812772d594f2096be485cd20a
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2022-12-30 02:56:01 +1300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 
-
+import Square from "./Square"
+import Input from "./Input"
+import { useState } from "react"
 
 function App() {
+  const [colorValue, setColorValue] = useState('')
+
   return (
-
     <div className="App">
-
-
+      <Square colorValue={colorValue}/>
+      <Input 
+      colorValue={colorValue}
+      setColorValue={setColorValue}
+      />
     </div>
   )
 
---
+++ src/Input.jsx
@@ -0,0 +1,19 @@
+
+
+const Input = ({ colorValue, setColorValue }) => {
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <label>Add Color Name:</label>
+      <input
+        type="text"
+        autoFocus
+        placeholder="Add color name"
+        required
+        value={colorValue}
+        onChange={(e) => setColorValue(e.target.value)}
+      />
+    </form>
+  )
+}
+
+export default Input
