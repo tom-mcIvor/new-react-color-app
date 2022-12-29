effb7900245c48906dc4e80c817b7d96862e2432
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2022-12-30 03:55:16 +1300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,22 @@ import { useState } from "react"
 
 function App() {
   const [colorValue, setColorValue] = useState('')
+  const [hexValue, setHexValue] = useState('')
+  const [isDarkText, setIsDarkText] = useState(true)
 
   return (
     <div className="App">
-      <Square colorValue={colorValue}/>
-      <Input 
-      colorValue={colorValue}
-      setColorValue={setColorValue}
+      <Square
+        colorValue={colorValue}
+        hexValue={hexValue}
+        isDarkText={isDarkText}
+      />
+      <Input
+        colorValue={colorValue}
+        setColorValue={setColorValue}
+        setHexValue={setHexValue}
+        isDarkText={isDarkText}
+        setIsDarkText={setIsDarkText}
       />
     </div>
   )
--- src/Input.jsx
+++ src/Input.jsx
@@ -1,17 +1,28 @@
+import colorNames from "colornames"
 
-
-const Input = ({ colorValue, setColorValue }) => {
+const Input = ({
+  colorValue, setColorValue, setHexValue, isDarkText, setIsDarkText
+}) => {
   return (
     <form onSubmit={(e) => e.preventDefault()}>
       <label>Add Color Name:</label>
       <input
-        type="text"
         autoFocus
+        type="text"
         placeholder="Add color name"
         required
         value={colorValue}
-        onChange={(e) => setColorValue(e.target.value)}
+        onChange={(e) => {
+          setColorValue(e.target.value)
+          setHexValue(colorNames(e.target.value))
+        }}
       />
+      <button
+        type='button'
+        onClick={() => setIsDarkText(!isDarkText)}
+      >
+        Toggle Text Color
+      </button>
     </form>
   )
 }
